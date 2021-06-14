0385e49fc4e40a22d07c2fa4385a9e0a453a2d5a
mdiakhate12 <mdiakhate1297@gmail.com>
2021-06-14 15:23:17 +0000

    diaf

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd62902726abb1c679e044bcc69e9b810'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '3ccf7dac-1bd6-4c38-b204-8ec69330ad68'
+};
